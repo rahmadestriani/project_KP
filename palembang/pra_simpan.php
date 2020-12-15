02fa9147e903f1c845ab3d6c80007c3891ca8c15
rahmadestriani <rahma.destriani03@gmail.com>
2020-12-15 11:06:37 +0700

    Revert "pra_plg"
    This reverts commit 26182100bc791c9c5df7c4dd8adb6730fbf60d5c.

--- palembang/pra_simpan.php
+++ palembang/pra_simpan.php
@@ -4,21 +4,28 @@
 	$no_jaringan = $_POST['no_jaringan'];
 	$pelanggan = $_POST['pelanggan'];
 	$lokasi = $_POST['lokasi'];
+	$area = 'palembang';
 	$provider = $_POST['provider'];
 	$no_kartu= $_POST['no_kartu'];
+	$jenis_provider= $_POST['jenis_provider'];
 	$status_layanan= $_POST['status_layanan'];
 	$perangkat= $_POST['perangkat'];
+	if (empty($_POST['awal_pengisian'])) {
+		$awal_pengisian = '0000-00-00';
+		$masa_aktif = '0000-00-00';
+	}
+	else {
 	$awal_pengisian= $_POST['awal_pengisian'];
-	$awal_pengisian= date('Y-m-d', strtotime($awal_pengisian));
-	$masa_aktif= date('Y-m-d', strtotime('+1 month', strtotime($awal_pengisian)));
+	$masa_aktif= date('y-m-d', strtotime('+1 month', strtotime($awal_pengisian)));
+	}
 	$status= $_POST['status'];
 
 
-	$query = "INSERT INTO plg_pra (`no_jaringan`,`pelanggan`,`lokasi`,`provider`,`no_kartu`,`status_layanan`,`perangkat`,`awal_pengisian`,`masa_aktif`,`status`) VALUES ('$no_jaringan','$pelanggan','$lokasi','$provider','$no_kartu','$status_layanan','$perangkat','$awal_pengisian','$masa_aktif','$status')";
+	$query = "INSERT INTO rekap_kartu (`no_jaringan`,`pelanggan`,`lokasi`,`area`,`provider`,`no_kartu`,`jenis_provider`,`status_layanan`,`perangkat`,`awal_pengisian`,`masa_aktif`,`status`) VALUES ('$no_jaringan','$pelanggan','$lokasi','$area','$provider','$no_kartu','$jenis_provider','$status_layanan','$perangkat','$awal_pengisian','$masa_aktif','$status')";
 	$hasil = mysqli_query ($koneksi,$query);
 
 	if (!$hasil){
 		die("Penyimpanan gagal!!!");
 	}
-	header('location:pra_plg.php');
+	header('location:palembang.php');
 ?>
--- palembang/pra_ubah.php
+++ palembang/pra_ubah.php
@@ -2,7 +2,7 @@
   include ('../koneksi.php');
   $no_jaringan = $_GET ['no_jaringan'];
 
-    $query = "SELECT * FROM plg_pra where no_jaringan='$no_jaringan'";
+    $query = "SELECT * FROM rekap_kartu where no_jaringan='$no_jaringan'";
     $hasil = mysqli_query ($koneksi, $query);
 
     $plg=mysqli_fetch_array($hasil);
@@ -10,11 +10,10 @@
     $lokasi = $plg['lokasi'];
     $provider = $plg['provider'];
     $no_kartu= $plg['no_kartu'];
+    $jenis_provider= $plg['jenis_provider'];
     $status_layanan= $plg['status_layanan'];
     $perangkat= $plg['perangkat'];
     $awal_pengisian= $plg['awal_pengisian'];
-    $awal_pengisian= date('Y-m-d', strtotime($awal_pengisian));
-    $masa_aktif= date('Y-m-d', strtotime('+1 month', strtotime($awal_pengisian)));
     $status= $plg['status'];
 ?>
 
@@ -89,7 +88,7 @@
                   </div>
                   <div class="card-body">
 
-                    <form role="form" action="pra_update.php" method="POST">
+                    <form role="form" action="ubah.php" method="POST">
                       <div class="form-group row">
                         <label class="col-md-3 form-control-label">No Jaringan</label>
                         <div class="col-md-9">
@@ -126,12 +125,25 @@
                         </div>
                     </div>
 
-                     <div class="form-group row">
+					           <div class="form-group row">
                         <label class="col-md-3 form-control-label">No Kartu</label>
                         <div class="col-md-9">
                           <input name="no_kartu" type="text" class="form-control" value="<?php echo $no_kartu; ?>">
                         </div>
                       </div>
+
+
+                      <div class="form-group row">
+                        <label class="col-md-3 form-control-label">Jenis Provider</label>
+                        <div class="col-md-9 select mb-3">
+                           <select name="jenis_provider" class="form-control" onchange="if (this.selectedIndex==1){document.getElementById('tampil_tanggal').style.display= 'inline' } else { document.getElementById('tampil_tanggal').style.display = 'none' };">
+
+                            <option <?php echo ($jenis_provider == 'Pascabayar') ? "selected": "" ?>>Pascabayar</option>
+                            <option <?php echo ($jenis_provider == 'Prabayar') ? "selected": "" ?>>Prabayar</option>
+                            
+
+                          </select>
+                        </div></div>
 
                       <div class="form-group row">
                         <label class="col-md-3 form-control-label">Status Layanan</label>
@@ -163,26 +175,28 @@
                             <option <?php echo ($status == 'Standby') ? "selected": "" ?>>Standby</option>
                           </select>
                     </div>
-                  </div>
-
-                 
+                	</div>
+                  <span id="tampil_tanggal" style="display: none;">
                   <div class="form-group row">
+                       
                         <label class="col-md-3 form-control-label">Awal Pengisian Kuota</label>
                         <div class="col-md-9">
-                          <input name="awal_pengisian" type="date" class="form-control" value="<?php echo $awal_pengisian; ?>">
+                          <input type="date" class="form-control"
+                          value="<?php echo $awal_pengisian; ?>">
                         </div>
-                  </div>
+                      </div>
+                    </span>
 
                
                         <center>
-                          <a href="pra_plg.php" class="btn btn-secondary">Cancel</a>
+                          <a href="palembang.php" class="btn btn-secondary">Cancel</a>
                           <button type="submit" class="btn btn-success">Save</button>
                         </center>
 
                     </form>
                 </div>
               </div>
-              </div>  
+              </div>	
         </section>
 
            
